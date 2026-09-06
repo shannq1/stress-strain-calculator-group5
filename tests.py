4b9c2136fe14a2a9d151204d9b9a96665c9f09cd
gabrielponceagmata-alt <gabrielponce.agmata@ust.edu.ph>
2026-09-06 23:48:33 +0800

    feat: Restructured StressStrainCalculator

--- tests.py
+++ tests.py
@@ -2,7 +2,6 @@ import unittest
 from material import Material
 from properties import StressStrainTest
 from database import CalculatorSession, TestRecord
-import unittest
 
 try:
     from material import Material
